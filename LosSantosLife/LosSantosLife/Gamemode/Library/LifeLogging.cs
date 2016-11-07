d57f20339eef5782696888d1ed85e8889f802813
Ryan Kingstone <ryan.kingstone@outlook.com>
2016-11-07 18:02:37 +0100

    Added the base for Los Santos Life.

---
+++ LosSantosLife/LosSantosLife/Gamemode/Library/LifeLogging.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using GTANetworkServer;
+
+public static class LifeLogging
+{
+    /// <summary>
+    /// Sends a default log, allowing you to choose colour.
+    /// </summary>
+    /// <param name="logText">log text</param>
+    /// <param name="logType">ConsoleColor</param>
+    public static void Log(string logText, LogType logType = LogType.Default)
+    {
+        switch (logType)
+        {
+            default: Console.ForegroundColor = ConsoleColor.White;
+                break;
+            case LogType.Default: Console.ForegroundColor = ConsoleColor.White;
+                break;
+            case LogType.Info:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case LogType.Debug:
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                break;
+            case LogType.LoggedDebug:
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                break;
+            case LogType.Warning:
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                break;
+            case LogType.Critical:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+        }
+        Console.WriteLine("[LIFE] " + DateTime.Now + ": " + logText);
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+}
+
+public enum LogType
+{
+    Default,
+    Info,
+    Debug,
+    LoggedDebug,
+    Warning,
+    Critical,
+}
---
+++ LosSantosLife/LosSantosLife/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LosSantosLife")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LosSantosLife")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8dcb44e6-83a8-4445-b743-2710d11e3fb8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
